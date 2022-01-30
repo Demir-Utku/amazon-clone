e39bd70d27bc4540ddb4c1db5b366d13650ad5b7
Utku Demir <utkdmr1@gmail.com>
2022-01-31 00:06:58 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+
+const App = () => (
+  <div className="container">
+    <h2>Hello World!</h2>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
